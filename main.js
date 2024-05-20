ea40d3d2f4de0445c473eb7f31778b25ae55b468
mohamed bekrine <mohamed.bekrine.mb@gmail.com>
2024-05-20 19:42:41 +0100

    double_screen

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
 
-const card_warpper = document.querySelectorAll('.scroller_warpper')
+const scrollerWarpper = document.querySelectorAll('.scroller_warpper')
 
 let persent=0
 
 
 
 function imagesPosition(){
-    const htmlElement = document.documentElement   
-    const percenOfScreenHeightScroll = htmlElement.scrollTop / htmlElement.clientHeight
-    htmlElement.style.setProperty('--scroll',Math.min(percenOfScreenHeightScroll * 50 ,200))
-    persent=Math.min(percenOfScreenHeightScroll * 50 ,200)
-    card_warpper.forEach((card,index)=>{
-        card.style.transform = `translateY(-${index * 25 +100 - persent}%)`
+
+    let count = 0
+    scrollerWarpper.forEach((card,index)=>{
+        // card.style.transform = `translateY(-${index * 25 +100 - persent}%) ;`
+        if(card.dataset.direction == 'sroller_left'){
+            console.log(count);
+            card.style.translate = `0 -${count * 25 + 100 - persent}%`
+            count +=1    
+        }else{
+            card.style.translate = `0 -${count * 25 + 100 - persent}%`
+        }
     })    
 }
 
+function imagesPositionTranslate(){
+    const htmlElement = document.documentElement   
+    const percenOfScreenHeightScroll = htmlElement.scrollTop / htmlElement.clientHeight
+    // htmlElement.style.setProperty('--scroll',Math.min(percenOfScreenHeightScroll * 50 ,200))
+    persent=Math.min(percenOfScreenHeightScroll * 50 ,200)
+    let count = 0
+    let count2 = 0
 
-document.addEventListener('scroll',imagesPosition)
+    scrollerWarpper.forEach((card,index)=>{
+        // card.style.transform = `translateY(-${index * 25 +100 - persent}%) ;`
+        if(card.dataset.direction == 'sroller_left'){
+            card.style.translate = `0 -${count * 25 + 100 - persent}%`
+            
+            count +=1    
+          
+        }else{
+            card.style.translate = `0 -${count2 * 25 + 100 - persent}%`
+            count2+=1
+        }
+    }) 
+}
+
+
+document.addEventListener('scroll',imagesPositionTranslate)
 
 imagesPosition()
